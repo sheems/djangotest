613e985e148a0dae1951d09c8098fbe0706a4eb9
Ron R <sheems@yandex.ru>
2018-08-03 15:58:29 +0300

    Add all this files

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse("Hello World!")
 
 urlpatterns = [
+    url(r'^$', hello),
     url(r'^admin/', admin.site.urls),
 ]
