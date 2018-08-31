7b0b2a99de9ab64af0a4f7260085dc0b85ecebfe
Ron <sheems@yandex.ru>
2018-08-31 16:26:14 +0300

    please enter the commit message for your changes. Lines starting sd

--- instructors/models.py
+++ instructors/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Instructor(models.Model):
-    name = models.CharField(max_length=64)
-    surname = models.CharField(max_length=64)
-    course = models.CharField(max_length=64)
+    name = models.CharField(verbose_name='Instructor Name', max_length=255, help_text="This is name")
+    surname = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    course = models.CharField(max_length=255)
+    slug = model.Slugsfield(unique=True)
+    is_active = models.BooleanField(default=True)
+    gender = models.CharField(max_length=1, choices=('m', 'Male'), ('f', 'Female'))
